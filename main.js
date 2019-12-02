4d19340c50e6df26ac0a6bc51526943cab130b25
Pablo Batista <55573363+pabrul@users.noreply.github.com>
2019-12-02 01:30:32 -0300

    main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+   $(document).ready(function() {
+    $('#got').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            'copyHtml5',
+            'excelHtml5',
+            'csvHtml5',
+            'pdfHtml5'
+        ]
+    } );
+} );
+ 
